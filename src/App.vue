dc4e505d0430b868fa923ee4b3c0dfa3d0761b55
lui5gl <lui5gl@outlook.com>
2025-02-02 09:23:52 -0600

    Add Browse component for album details and integrate with Player for improved layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import Browse from "./modules/browse/browse.vue";
 import Navigation from "./modules/navigation/navigation.vue";
 import Player from "./modules/player/player.vue";
 
@@ -13,64 +14,7 @@ const handleTogglePlay = () => {
 <template>
   <main class="flex flex-col min-h-screen bg-neutral-900">
     <Navigation />
-    <main class="flex flex-col gap-5">
-      <section class="bg-gradient-to-b from-sky-500 pt-10">
-        <div
-          class="w-full max-w-2xl flex gap-5 justify-center items-end mx-auto"
-        >
-          <img src="/images/The-Beatles.png" alt="" class="w-full max-w-64" />
-          <div class="flex flex-col gap-3">
-            <p class="text-neutral-100">Álbum</p>
-            <p class="text-neutral-100 font-bold text-5xl">
-              Abbev Road (Remastered)
-            </p>
-            <p class="text-neutral-200">
-              The Beatles • 1969 • 17 canciones, 47 min 29 s
-            </p>
-          </div>
-        </div>
-      </section>
-
-      <section class="w-full max-w-2xl mx-auto">
-        <button
-          @click="handleTogglePlay"
-          class="hover:bg-green-500 hover:scale-110 bg-green-400 p-2 rounded-full transition-all duration-75 cursor-pointer"
-        >
-          <img
-            v-if="isPlaying === true"
-            src="/icons/player/play.png"
-            width="24"
-            height="24"
-            alt="player play"
-          />
-
-          <img
-            v-if="isPlaying === false"
-            src="/icons/player/pause.png"
-            width="24"
-            height="24"
-            alt="player pause"
-          />
-        </button>
-      </section>
-
-      <section class="w-full max-w-2xl mx-auto">
-        <table class="text-neutral-400 w-full grid-cols-3">
-          <thead>
-            <td>#</td>
-            <td>Título</td>
-            <td>
-              <img
-                src="/icons/player/clock.png"
-                alt="Duration"
-                width="16"
-                height="16"
-              />
-            </td>
-          </thead>
-        </table>
-      </section>
-    </main>
+    <Browse :is-playing="isPlaying" @toggle-play="handleTogglePlay" />
     <Player :is-playing="isPlaying" @toggle-play="handleTogglePlay" />
   </main>
 </template>
---
+++ src/modules/browse/browse.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+defineProps({
+  isPlaying: Boolean,
+});
+
+const emit = defineEmits(["togglePlay"]);
+
+const handleTogglePlay = () => {
+  emit("togglePlay");
+};
+</script>
+
+<template>
+  <main class="flex flex-col gap-5">
+    <section class="bg-gradient-to-b from-sky-500 pt-10">
+      <div class="w-full max-w-2xl flex gap-5 justify-center items-end mx-auto">
+        <img src="/images/The-Beatles.png" alt="" class="w-full max-w-64" />
+        <div class="flex flex-col gap-3">
+          <p class="text-neutral-100">Álbum</p>
+          <p class="text-neutral-100 font-bold text-5xl">
+            Abbev Road (Remastered)
+          </p>
+          <p class="text-neutral-200">
+            The Beatles • 1969 • 17 canciones, 47 min 29 s
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section class="w-full max-w-2xl mx-auto">
+      <button
+        @click="handleTogglePlay"
+        class="hover:bg-green-500 hover:scale-110 bg-green-400 p-2 rounded-full transition-all duration-75 cursor-pointer"
+      >
+        <img
+          v-if="isPlaying === true"
+          src="/icons/player/play.png"
+          width="24"
+          height="24"
+          alt="player play"
+        />
+
+        <img
+          v-if="isPlaying === false"
+          src="/icons/player/pause.png"
+          width="24"
+          height="24"
+          alt="player pause"
+        />
+      </button>
+    </section>
+
+    <section class="w-full max-w-2xl mx-auto">
+      <table class="text-neutral-400 w-full grid-cols-3">
+        <thead>
+          <td>#</td>
+          <td>Título</td>
+          <td>
+            <img
+              src="/icons/player/clock.png"
+              alt="Duration"
+              width="16"
+              height="16"
+            />
+          </td>
+        </thead>
+      </table>
+    </section>
+  </main>
+</template>
